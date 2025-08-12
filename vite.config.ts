d7d1d78c072d22932d005914e3b66f1e1884d7fe
AdeelNiazi00 <adeel.niazi@binatedigital.com>
2025-08-12 23:50:10 +0500

    chore: add allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -41,6 +41,7 @@ export default defineConfig({
     host: true,
     port: 5173,
     strictPort: true,
+    allowedHosts: '*',
     proxy: {
       '/api': {
         target: 'https://getta-api-new.deployment-uat.com/api/v1/',
