237f2d7d3ce499e470fa91f60ae617146e364543
aneebkhan151 <aneebullah.niazi@binatedigital.com>
2025-08-13 02:39:53 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
     host: true,
     port: 5173,
     strictPort: true,
-    allowedHosts: 'getta-web.deployment-uat.com',
+    allowedHosts: ['getta-web.deployment-uat.com'],
     proxy: {
       '/api': {
         target: 'https://getta-api-new.deployment-uat.com/api/v1/',
